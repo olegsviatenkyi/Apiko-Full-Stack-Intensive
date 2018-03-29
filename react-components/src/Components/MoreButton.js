6704f871873ecb712ad572c46253a3f5bab6a57d
olegsviatenkyi <33761111+olegsviatenkyi@users.noreply.github.com>
2018-03-29 12:22:16 +0300

    Update MoreButton.js

--- react-components/src/Components/MoreButton.js
+++ react-components/src/Components/MoreButton.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import PostList from "./PostList.js";
 import data from '../data.js';
 
-const MoreButton = () => {
+class MoreButton extends React.Component {
+	render() {
+	const {click} = this.props;
 	return (
 		<div>
-		
+			<button onClick={click}>More!</button>
 		</div>
-	)
+	)}
 };
 
 export default MoreButton;
