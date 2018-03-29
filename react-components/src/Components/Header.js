904989f5db18e5007424fd1bd26aa9e6a9129c6e
olegsviatenkyi <oleg160183@gmail.com>
2018-03-29 11:07:51 +0300

    Added files in react-components

---
+++ react-components/src/Components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+
+const Header = () => {
+	return (
+	<header className="App-header">
+		<img src={logo} className="App-logo" alt="logo" />
+		<h1 className="App-title">Full-Stack Intensive</h1>
+		<h2>React. Components</h2>
+	</header>
+	);
+};
+export default Header;
